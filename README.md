f06aed3bdbb5efeb90affcc49ae2d1d2b4826f2d
Daniel Haskin <djhaskin987@gmail.com>
2017-09-21 19:42:02 -0600

    Finish numberplz

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # numberplz
-A simple app that returns an auto incremented integer every time a GET request is sent to it.
+A simple web app that returns an auto incremented integer every time a GET
+request is sent to it.
 
 To restart the count, simply restart the app.
+
+This is kinda cool. It allows you to run a web service which, for example,
+can give each microservice that starts up and asks for a number from it a
+different ID. The use case I can see for this is when different agents need to
+know who they are in relation to each other for a short-lived job, such as a
+kubernetes Job.
+
+Pull and feature requests welcome! and thanks to Jason Reicheneker for the
+idea :)
+
+# Install
+
+Install go and then just run `go get github.com/djhaskin987/numberplz`.
+
+# Build
+
+If you're more interested in building it, check it out to the folder
+`$GOPATH/github.com/djhaskin987/numberplz`.
---
+++ main.go
@@ -0,0 +1,34 @@
+// Copyright © 2017 Daniel Jay Haskin <djhaskin987@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"net/http"
+)
+
+var number *big.Int = big.NewInt(0)
+var incrementer *big.Int = big.NewInt(1)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s\n", number.Text(10))
+	number.Add(number, incrementer)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
